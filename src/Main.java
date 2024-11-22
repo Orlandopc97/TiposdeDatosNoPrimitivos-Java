95454bfe9426fbfa4bbb3c4ce0f95af011e824fd
Orlandopc97 <orlandopc1797@gmail.com>
2024-11-22 13:24:36 -0600

    Achivos de Típos de datos no primitivos

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //null = pedazo de memoria totalmente vacia no almacena nada
+
+        //integer(entero) alamacena numeros y null
+        Integer numero = null;
+        //tienen metodos
+        numero = 1000;
+
+        String  palabra= "Hola?";
+
+        System.out.println(numero);
+        System.out.println();
+
+
+          /*Tipos de datos no primitivos = TIPOS DE REFERENCIA por que se refieren a objetos,
+
+          = Tipos Objeto (con métodos, necesitan una invocacion para ser creados)
+
+          String
+
+          Byte
+          Short
+          Integer
+          Long              Se inician con mayuscula a diferencia de algunos primitivos
+          Float             Equivalentes a los tipos primitivos pero como objetos
+          Double
+          Character
+          Boolean
+
+          String
+          Scanner           Tipos de la biblioteca estandar de java
+          TreeSet
+          ArrayList
+
+          Taxi
+          Autobus           Cualquiera que se nos ocurra,
+          Tranvia           Tipos definidos por el programador
+
+          arrays            Serie de elementos o formacion tipo vector o matriz.
+                            Lo consideraremos un objeto especial que carece de metodos
+
+        *Los tipos no primitivos los crea el programador y java no los define (excepto el String)
+
+        *Se pueden utilizar para llamar a metodos para realizar determinadas operaciones
+        */
+    }
+}
